beda64221504234cb54ecb492d21b97c95359fb2
Crafter-Y <mrcrafter.yt1@gmail.com>
2025-11-02 00:40:53 +0100

    fixing leaking database connections

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,7 @@
 import { expo } from "@better-auth/expo";
-import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
-const prisma = new PrismaClient();
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
