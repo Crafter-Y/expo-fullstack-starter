d5ea54df56ab25def7796966566b9e00ba08f52f
Crafter-Y <Crafter-Y@users.noreply.github.com>
2025-11-19 22:15:48 +0000

    Deploy preview for PR 8 🛫

---
+++ storybook/pr-8/assets/formatter-OMEEQ6HG-CWA9mr64.js
@@ -0,0 +1,1 @@
+import{r as t,d as e}from"./WithTooltip-SK46ZJ2J-CGYrViA9.js";import{h as m}from"./iframe-kfoPhJBm.js";import"./preload-helper-Zf8nSx-t.js";import"./react-18-CzLHgyLB.js";import"./jsx-runtime-Bm1D-IlX.js";var o=m(t(),1),s=(0,o.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
