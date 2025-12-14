198e5256c2940cf09b4ed50946874a3190251a3c
Crafter-Y <Crafter-Y@users.noreply.github.com>
2025-12-14 18:18:59 +0000

    Remove preview for PR 14 🛬

--- storybook/pr-14/assets/formatter-QJ4M4OGQ-DBwi33xN.js
+++
@@ -1,1 +0,0 @@
-import{r as m,d as o}from"./DocsRenderer-GHJI37HO-D8Xj3Qt_.js";import{ad as t}from"./iframe-hpTVAJXy.js";import"./preload-helper-Zf8nSx-t.js";import"./react-18-fDKtpDAD.js";import"./jsx-runtime-CTkmVHle.js";import"./index-7AjwzS9j.js";var e=t(m(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:o(r));export{c as formatter};
--- storybook/pr-14/assets/jsx-runtime-CTkmVHle.js
+++
@@ -1,1 +0,0 @@
-import{ar as r}from"./iframe-hpTVAJXy.js";var s=r();export{s as j};
